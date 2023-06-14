0a2894c60be1f0d9f4a936a537e3753084eee7ff
feigebaobei <18515195415@163.com>
2023-06-15 00:38:42 +0800

    feat(renderer): 从路由中取数据

--- packages/renderer/src/app/app-routing.module.ts
+++ packages/renderer/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ':appKey/:env/:page', component: AppComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- packages/renderer/src/app/app.component.ts
+++ packages/renderer/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+let clog = console.log
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'renderer';
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((data: any) => {
+      clog('paramMap data', data)
+      clog('paramMap data', data.get('appKey'))
+      clog('paramMap data', data.get('env'))
+      clog('paramMap data', data.get('page'))
+    })
+    // this.route.queryParamMap.subscribe((data: any) => {
+    //   clog('queryParamMap data', data)
+    // })
+  }
 }
