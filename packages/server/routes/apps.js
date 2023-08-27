f795087afb214880054ad4ef19177ac4365f5ff2
feigebaobei <18515195415@163.com>
2023-08-27 21:45:15 +0800

    feat(server): 创建应用

--- packages/server/routes/apps.js
+++ packages/server/routes/apps.js
@@ -4,7 +4,7 @@ var router = express.Router();
 let bodyParser = require('body-parser');
 // const fsPromises = require('fs/promises')
 // const path = require('path')
-let {appsDb} = require('../mongodb');
+let {appsDb, usersDb} = require('../mongodb');
 const { rules } = require('../helper');
 // let md5 = require('md5');
 let clog = console.log
@@ -47,44 +47,92 @@ router.route('/')
       rules.required(req.body.name) && 
       rules.required(req.body.ulid) && 
       rules.isArray(req.body.members)
-      // rules.required(req.body.version) && rules.isVersion(req.body.version)
     ) {
+        // 保存应用
         let version = req.body.version || 0
-      //   clog('user', req.session)
-      //   clog('req', req.body, {
-      //     key: req.body.key,
-      //     name: req.body.name,
-      //     ulid: req.body.ulid,
-      //     theme: req.body.theme,
-      //     // version: req.body.version,
-      //     version,
-      //     owner: req.session.user.account,
-      //     members: req.body.members.slice(0, 4),
-      //     firstPage: '',
-      // })
-        appsDb.collection('apps').insertOne({
-            key: req.body.key,
-            name: req.body.name,
-            ulid: req.body.ulid,
-            theme: req.body.theme,
-            // version: req.body.version,
-            version,
-            owner: req.session.user.account,
-            members: req.body.members.slice(0, 4),
-            firstPage: '',
-        }).then((apps) => {
-            res.status(200).json({
-              code: 0,
-              message: "ok",
-              data: {},
-            })
+        let members = req.body.members.slice(0, 4)
+        // let cApp = appsDb.collection('apps').insertOne({
+        //     key: req.body.key,
+        //     name: req.body.name,
+        //     ulid: req.body.ulid,
+        //     theme: req.body.theme,
+        //     version,
+        //     owner: req.session.user.account,
+        //     members: req.body.members.slice(0, 4), // 最多有4个人。可优化为配置项。
+        //     firstPageUlid: '',
+        //     prevUlid: req.session.user.applications[req.session.user.applications.length - 1] || '',
+        //     nextUlid: '',
+        // })
+        let lastAppUlid = req.session.user.applications[req.session.user.applications.length - 1]
+        let appObj = [
+          {
+            updateOne: {
+              filter: {ulid: lastAppUlid},
+              update: {
+                $set: {nextUlid: req.body.ulid}
+              }
+            }
+          },
+          {
+            insertOne: {
+              document: {
+                key: req.body.key,
+                name: req.body.name,
+                ulid: req.body.ulid,
+                theme: req.body.theme,
+                version,
+                owner: req.body.owner,
+                members,
+                firstPageUlid: '',
+                prevUlid: lastAppUlid,
+                nextUlid: ''
+              }
+            }
+          }
+        ]
+        let cApp = appsDb.collection('apps').bulkWrite(appObj)
+        // 修改用户表中的数据
+        // let curUser = req.session.user
+        // let eUser = usersDb.collection('users').updateOne({
+        //   account: curUser.account,
+        // }, {$push: {applications: req.body.ulid}})
+        // insertOne
+        // replaceOne
+        // updateOne
+        // updateMany
+        // deleteOne
+        // deleteMany
+        let objArr = req.body.members.map(item => {
+          return {
+            updateOne: {
+              filter: { account: item },
+              update: { $addToSet: { applications: req.body.ulid }},
+            }
+          }
+        })
+        clog('boj', objArr)
+        let eUser = usersDb.collection('users').bulkWrite(objArr)
+        // todo 修改所有用户的applications
+        Promise.all([cApp, eUser])
+        .then(() => {
+          return res.status(200).json({
+            code: 0,
+            message: "ok",
+            data: {},
+          })
         }).catch((error) => {
-        res.status(200).json({
+          appsDb.collection('apps').deleteOne({ulid: req.body.ulid})
+          usersDb.collection('users').updateOne({account: req.session.user.account}, {
+            $set: {applications: req.session.user.applications}
+          })
+          res.status(200).json({
             code: 200200,
             message: "数据库出错",
             data: error,
           })
         })
+
+
     } else {
         res.status(200).json({
           code: 100100,
