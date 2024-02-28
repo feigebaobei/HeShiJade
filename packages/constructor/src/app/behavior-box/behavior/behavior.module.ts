c01479b79114e5c28ab809be3209a76a455f5d2f
feigebaobei <18515195415@163.com>
2024-02-29 07:33:05 +0800

    feat(constructor): 使用module处理行为

--- packages/constructor/src/app/behavior-box/behavior/behavior.module.ts
+++ packages/constructor/src/app/behavior-box/behavior/behavior.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BehaviorBoxComponent } from '../behavior-box.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BehaviorBoxComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    BehaviorBoxComponent
   ]
 })
 export class BehaviorModule { }
