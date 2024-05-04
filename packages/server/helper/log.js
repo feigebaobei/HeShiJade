9ceb8a462a9cc767ca36295c3edaebfcace9ecb7
feigebaobei <18515195415@163.com>
2024-05-04 21:09:11 +0800

    feat(server): 打通日志

---
+++ packages/server/helper/log.js
@@ -0,0 +1,14 @@
+const pino = require('pino')
+const path = require('path')
+
+let logger = pino({
+    name: 'logname',
+}, pino.destination(path.resolve(`${__dirname}`, `../log/access.log`)))
+
+console.log('logger', logger)
+
+module.exports = {
+    pino,
+    // logger,
+    logger,
+}
