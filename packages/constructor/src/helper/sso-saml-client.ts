9e74d1d8b174163650ff313836d607937a57fffd
feigebaobei <18515195415@163.com>
2024-05-10 10:41:58 +0800

    feat(constructor): 封装ssoclient包

---
+++ packages/constructor/src/helper/sso-saml-client.ts
@@ -0,0 +1,44 @@
+import { req } from './axios'
+// type
+import type { Method,RequestData,
+    ResponseData, } from './axios'
+import type { A, Ao, N, S } from "src/types/base"
+
+interface SsoClientParams {
+    idp: {
+        url: S
+        method: Method
+        data: A
+    }
+    sp: {
+        url: S
+        method: Method
+    }
+}
+
+let opReqObj = (url: S, method: Method, data: A): RequestData => {
+    let reqObj: A = {
+        url,
+        method,
+    }
+    switch(method) {
+        case 'get': 
+            reqObj.params = data
+            break;
+        default:
+            reqObj.data = data
+            break;
+    }
+    return reqObj
+}
+let login = (obj: SsoClientParams) => {
+    return req(opReqObj(obj.idp.url, obj.idp.method, obj.idp.data)).then((res: ResponseData) => {
+        return req(opReqObj(obj.sp.url, obj.sp.method, res.data))
+    })
+}
+export {
+    login
+}
+export type {
+    SsoClientParams
+}
