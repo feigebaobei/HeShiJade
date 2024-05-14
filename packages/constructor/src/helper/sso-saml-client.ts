93210f2ebddffc5523eeb96f56bd8ab47303a80b
feigebaobei <18515195415@163.com>
2024-05-15 07:09:18 +0800

    to(constructor): 尝试兼容promise&cb，但是被ts类型卡住了。

--- packages/constructor/src/helper/sso-saml-client.ts
+++ packages/constructor/src/helper/sso-saml-client.ts
@@ -2,7 +2,7 @@ import { req } from './axios'
 // type
 import type { Method,RequestData,
     ResponseData, } from './axios'
-import type { A, Ao, N, S } from "src/types/base"
+import type { A, Ao, F, N, S } from "src/types/base"
 
 interface SsoClientParams {
     idp: {
@@ -14,6 +14,19 @@ interface SsoClientParams {
         url: S
         method: Method
     }
+}
+interface SamlRes {
+    idpRes: {
+        code: N
+        message: S
+        data: A
+    }
+    spRes: {
+        code: N
+        message: S
+        data: A
+    }
+
 }
 
 let opReqObj = (url: S, method: Method, data: A): RequestData => {
@@ -31,7 +44,7 @@ let opReqObj = (url: S, method: Method, data: A): RequestData => {
     }
     return reqObj
 }
-let login = (obj: SsoClientParams) => {
+let _login = (obj: SsoClientParams) => {
     return req(opReqObj(obj.idp.url, obj.idp.method, obj.idp.data)).then((idpRes: ResponseData) => {
         return req(opReqObj(obj.sp.url, obj.sp.method, idpRes.data)).then((spRes) => {
             return {
@@ -41,8 +54,27 @@ let login = (obj: SsoClientParams) => {
         })
     })
 }
+
+// 这样可以实现promise&cb，但是使用该方法时ts类型不对。
+// type T = (obj: SsoClientParams) => Promise<SamlRes>
+// type G = (obj: SsoClientParams, cb: F) => void
+// let login: T | G = (obj, cb) => {
+//     if (cb) {
+//         _login(obj).then((SamlRes) => {
+//             cb(SamlRes)
+//         }).catch(() => {
+//             console.log('str')
+//         })
+//         return undefined
+//     } else {
+//         return _login(obj)
+//     }
+// }
+let login = _login
+
+
 export {
-    login
+    login,
 }
 export type {
     SsoClientParams
