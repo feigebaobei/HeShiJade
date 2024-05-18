ee0b44d225478808214c4ece3809c31f7a805b23
feigebaobei <18515195415@163.com>
2024-05-18 10:41:09 +0800

    feat(constructor): 创建ssoClient文件

--- packages/constructor/src/helper/config.ts
+++ packages/constructor/src/helper/config.ts
@@ -1,5 +1,6 @@
 import { A } from "src/types/base"
-import type { SsoClientParams } from "./sso-saml-client"
+// import type { SsoClientParams } from "./sso-saml-client"
+import type { Method, } from './axios'
 
 // 日后使用环境变量判断
 let ssoUrl = () => 'http://localhost:5020'
@@ -7,18 +8,36 @@ let serviceUrl = () => 'http://localhost:5000'
 const APPTOTALMAX = 10
 const PAGETOTALMAXOFAPP = 20
 const COMPONENTTOTALMAXOFPAGE = 30
-let ssoClientParams = (idpData: A): SsoClientParams => {
-    return {
-        idp: {
-            url: `${ssoUrl()}/users/saml`,
-            method: 'post',
-            data: idpData,
-        },
-        sp: {
-            url: `${serviceUrl()}/users/saml`,
-            method: 'post'
-        }
-    }
+// let ssoClientParams = (idpData: A): SsoClientParams => {
+//     return {
+//         idp: {
+//             url: `${ssoUrl()}/users/saml`,
+//             method: 'post',
+//             data: idpData,
+//         },
+//         sp: {
+//             url: `${serviceUrl()}/users/saml`,
+//             method: 'post'
+//         }
+//     }
+// }
+let ssoClientConfig = {
+    idp: {
+        loginUrl: `${ssoUrl()}/users/login`,
+        signUrl: `${ssoUrl()}/users/sign`,
+        loginMethod: 'post' as Method,
+        signMethod: 'post' as Method,
+        verificationUrl: `${ssoUrl()}/users/verification`,
+        verificationMethod: 'post' as Method,
+    },
+    sp: {
+        loginUrl: `${serviceUrl()}/users/login`,
+        loginMethod: 'post' as Method,
+        signUrl: `${serviceUrl()}/users/sign`,
+        signMethod: 'post' as Method,
+        logoutUrl: `${serviceUrl()}/users/logout`,
+        logoutMethod: 'delete' as Method,
+    },
 }
 
 export {
@@ -27,5 +46,6 @@ export {
     APPTOTALMAX,
     PAGETOTALMAXOFAPP,
     COMPONENTTOTALMAXOFPAGE,
-    ssoClientParams,
+    // ssoClientParams,
+    ssoClientConfig,
 }
--- packages/constructor/src/helper/sso-saml-client.ts
+++ packages/constructor/src/helper/sso-saml-client.ts
@@ -1,6 +1,6 @@
 import { req } from './axios'
 // type
-import type { Method,RequestData,
+import type { Method, RequestData,
     ResponseData, } from './axios'
 import type { A, Ao, F, N, S } from "src/types/base"
 
@@ -26,16 +26,16 @@ interface SamlRes {
         message: S
         data: A
     }
-
 }
 
-let opReqObj = (url: S, method: Method, data: A): RequestData => {
+let opReqObj = (url: S, method: Method, data?: A): RequestData => {
     let reqObj: A = {
         url,
         method,
     }
     switch(method) {
         case 'get': 
+        case 'delete': 
             reqObj.params = data
             break;
         default:
@@ -72,10 +72,109 @@ let _login = (obj: SsoClientParams) => {
 // }
 let login = _login
 
+interface Idp {
+    loginUrl: S
+    signUrl: S
+    loginMethod: Method
+    signMethod: Method
+    verificationUrl: S
+    verificationMethod: Method
+}
+interface Sp {
+    loginUrl: S
+    loginMethod: Method
+    signUrl: S
+    signMethod: Method
+    logoutUrl: S
+    logoutMethod: Method
+}
+interface SsoClient<T = ResponseData> {
+    idp: Idp
+    sp: Sp
+    loginIdp: (p: A) => Promise<T>
+    loginSp: (p: A) => Promise<T>
+    login: (p: A) => Promise<{idpRes: A, spRes: A}>
+    logoutSp: (p?: A) => Promise<T>
+    signIdp: (p: A) => Promise<T>
+    sendVerification: (p: A) => Promise<ResponseData>
+}
+let obj = Object.create({}, {
+    loginIdp: {
+        value: function (data: A): Promise<ResponseData> {
+            return req(opReqObj((this as unknown as SsoClient).idp.loginUrl, (this as unknown as SsoClient).idp.loginMethod, data))
+        }
+    },
+    loginSp: {
+        value: function (data: A): Promise<ResponseData> {
+            return req(opReqObj((this as unknown as SsoClient).sp.loginUrl, (this as unknown as SsoClient).sp.loginMethod, data))
+        }
+    },
+    login: {
+        value: function (data: A): Promise<{idpRes: A, spRes: A}> {
+            return (this as unknown as SsoClient).loginIdp(data).then((idpRes) => {
+                if (idpRes.code === 0) {
+                    return (this as unknown as SsoClient).loginSp(idpRes.data).then((spRes) => {
+                        if (spRes) {
+                            return {
+                                idpRes,
+                                spRes,
+                            }
+                        } else {
+                            return Promise.reject()
+                        }
+                    })
+                } else {
+                    return Promise.reject()
+                }
+            })
+        }
+    },
+    logoutSp: {
+        value: function (data?: A): Promise<ResponseData> {
+            return req(opReqObj((this as unknown as SsoClient).sp.logoutUrl, (this as unknown as SsoClient).sp.logoutMethod, data))
+        }
+    },
+    signIdp: {
+        value: function (data: A): Promise<ResponseData> {
+            return req(opReqObj((this as unknown as SsoClient).idp.signUrl, (this as unknown as SsoClient).idp.signMethod, data))
+        }
+    },
+    sendVerification: {
+        value: function (data: A): Promise<ResponseData> {
+            return req(opReqObj((this as unknown as SsoClient).idp.verificationUrl, (this as unknown as SsoClient).idp.verificationMethod, data))
+        }
+    }
+})
+let createSsoClient = (idp: Idp, sp: Sp): SsoClient => Object.create(obj, {
+    idp: {
+        value: {
+            loginUrl: idp.loginUrl || '',
+            signUrl: idp.signUrl || '',
+            loginMethod: idp.loginMethod || '',
+            signMethod: idp.signMethod || '',
+            verificationUrl: idp.verificationUrl || '',
+            verificationMethod: idp.verificationMethod || '',
+        }
+    },
+    sp: {
+        value: {
+            loginUrl: sp.loginUrl || '',
+            loginMethod: sp.loginMethod || '',
+            signUrl: sp.signUrl || '',
+            signMethod: sp.signMethod || '',
+            logoutUrl: sp.logoutUrl || '',
+            logoutMethod: sp.logoutMethod || '',
+        }
+    },
+})
+
 
 export {
-    login,
+    // login,
+    createSsoClient,
 }
 export type {
-    SsoClientParams
+    // SsoClientParams
+    // createSsoClient
+    SsoClient,
 }
