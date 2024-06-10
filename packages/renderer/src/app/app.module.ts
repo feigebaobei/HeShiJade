7e41c8690e4c85c49a7b911a901fe820347a7d00
feigebaobei <18515195415@163.com>
2024-06-10 22:30:49 +0800

    fix(renderer): 安装新的依赖解决了inject()的问题

--- packages/renderer/src/app/app.module.ts
+++ packages/renderer/src/app/app.module.ts
@@ -1,17 +1,18 @@
+// module
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 // import { BrowserAnimationsModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DevUIModule } from 'ng-devui';
 import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentsModule } from './components/components.module';
+// components
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { HttpClientModule } from '@angular/common/http';
 import { LayoutComponent } from './layout/layout.component';
 import { PageListComponent } from './page/page-list/page-list.component';
-import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
