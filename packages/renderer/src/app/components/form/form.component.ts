6a30a8a396e88e4868a71275e526c2a7a83a92f3
feigebaobei <18515195415@163.com>
2024-08-04 21:12:06 +0800

    feat(renderer): 使用配置信息渲染form

--- packages/renderer/src/app/components/form/form.component.ts
+++ packages/renderer/src/app/components/form/form.component.ts
@@ -1,7 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { DataService } from 'src/app/service/data.service';
 import {shareEvent} from 'src/helper';
-import type { A } from 'src/types/base';
+import type { A, S } from 'src/types/base';
 
 let clog = console.log
 
@@ -10,11 +10,38 @@ let clog = console.log
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.sass']
 })
-export class FormComponent {
+export class FormComponent implements OnInit, OnChanges {
   @Input() data: A
-  constructor(private dataService: DataService) {}
+  props: A
+  items: A
+  rules: A
+  first: S
+  constructor(private dataService: DataService) {
+    this.props = {} // this.data.props
+    this.items = {} //this.data.items
+    this.first = 'str'
+    this.rules = {
+      validators: [
+        { required: true },
+      ],
+      message: 'Enter a'
+    }
+  }
+  ngOnInit() {
+    this.props = this.data.props
+    this.items = this.data.items
+    clog('props', this.props, this.items)
+  }
+  ngOnChanges() {
+    clog('ngOnChanges')
+  }
   submitClickH() {
-    this.dataService.req(this.data.props.url, 'post', {}).then(res => {
+    clog(this.data)
+    let data: A = {}
+    this.data.items.forEach((item: A) => {
+      data[item.key] = item.value
+    })
+    this.dataService.req(this.data.props.url, 'post', data).then(res => {
       let eventArr = this.data.behavior.filter((item: A) => item.event === 'submit')
       eventArr.forEach((item: A) => {
         if (res.code === 0) {
