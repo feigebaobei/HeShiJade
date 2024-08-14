345dadabdabdbc3bea77054d8850770f6083a846
feigebaobei <18515195415@163.com>
2024-08-15 07:30:34 +0800

    feat(constructor): tabs支持添加子组件

--- packages/constructor/src/app/components/tabs/tabs.component.ts
+++ packages/constructor/src/app/components/tabs/tabs.component.ts
@@ -1,8 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { PageService } from 'src/app/service/page.service';
+import { ComponentService } from 'src/app/service/component.service';
+import { createChildKey, initComponentMeta } from 'src/helper';
+import { compatibleArray } from 'src/helper'
 // type
 import type { Component as Comp, ComponentMountItems } from 'src/types/component';
 import type { ULID } from 'src/types';
-import { S } from 'src/types/base';
+import type { N, S } from 'src/types/base';
+import type { Page } from 'src/types/page';
+import type { DropEvent } from 'ng-devui';
 
 interface TabsData {
   props: Comp['props']
@@ -20,11 +26,55 @@ interface TabsData {
 })
 export class TabsComponent implements OnInit{
   @Input() data!: TabsData
-  // activeTab: S = 'id'
-  constructor() {
-    // this.activeTab = 'tab1' // this.data.props['activeTab']
+  compObj: {[k: S]: Comp[]}
+  curPage: Page
+  createChildKey: typeof createChildKey
+  compatibleArray: typeof compatibleArray
+  constructor(
+    private pageService: PageService,
+    private componentService: ComponentService,
+  ) {
+    this.compObj = {}
+    this.curPage = this.pageService.getCurPage()!
+    this.createChildKey = createChildKey
+    this.compatibleArray = compatibleArray
   }
   ngOnInit() {
-    // this.activeTab = this.data.props['activeTab'] || 'id'
+    this.compObj = {}
+    let tree = this.componentService.getTree(this.curPage.ulid)
+    if (tree) {
+      let node = tree.find(this.data.ulid)
+      Object.entries(node?.value.slots || {}).forEach(([k, v]) => {
+        let key = createChildKey('slots', k, 'component')
+        let childNode = tree.find(v)!
+        this.compObj[key] = childNode.toArray()
+      })
+    }
+  }
+  getChildrenComponent(itemIndex: N | S) {
+    let key = createChildKey('slots', itemIndex, 'component')
+    return this.compObj[key] || []
+  }
+  dropH(e: DropEvent, itemIndex: N) {
+    let comp: Comp
+    // 因items的id会变。index不会变。所以使用index为key.
+    let key = createChildKey('slots', itemIndex, 'component')
+    comp = initComponentMeta(
+      e.dragData.item.componentCategory,
+      this.curPage.appUlid, this.curPage.ulid,
+      this.compObj[key]?.length ? this.compObj[key][this.compObj[key].length - 1].ulid : '',
+      '', this.data.ulid,
+      {area: 'slots', slotKey: String(itemIndex)}
+    )
+    if (this.compObj[key]?.length) {
+      this.compObj[key].push(comp)
+    } else {
+      this.compObj[key] = [comp]
+    }
+    this.componentService.mountComponent(this.curPage.ulid, comp)
+    this.componentService.reqPostCompListByPage(comp)
+  }
+  deleteComponentByUlidH(ulid: ULID, index : N) {
+
   }
 }
