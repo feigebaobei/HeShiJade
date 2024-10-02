9d96f9f7ae236e066bde1fae842a2d8afc8b684d
feigebaobei <18515195415@163.com>
2024-10-02 08:03:34 +0800

    chore(server): 注释不用的代码

--- packages/server/routes/plugins.js
+++ packages/server/routes/plugins.js
@@ -90,90 +90,90 @@ router.route('/')
   res.send('delete')
 })
 
-router.route('/:key')
-.options(cors.corsWithOptions, (req, res) => {
-  res.sendStatus(200)
-})
-.get(cors.corsWithOptions, (req, res) => {
-  // req.params.key
-  // res.status(200).json({   // x
-  //   code: 0,
-  //   message: '',
-  //   data: {
-  //     profile: {
-  //       key: 'key',
-  //       authorEmail: 'authorEmail',
-  //       authorName: 'authorName',
-  //     },
-  //     hooks: {
-  //       rootInstancePost: "() => {return 'rootInstancePost'}",
-  //       pageInstancePost: () => {return 'pageInstancePost'},
-  //       componentInstancePost: () => {return 'componentInstancePost'},
-  //     },
-  //     fnx: () => {
-  //       clog('fnx')
-  //     }
-  //   }
-  // })
+// router.route('/:key')
+// .options(cors.corsWithOptions, (req, res) => {
+//   res.sendStatus(200)
+// })
+// .get(cors.corsWithOptions, (req, res) => {
+//   // req.params.key
+//   // res.status(200).json({   // x
+//   //   code: 0,
+//   //   message: '',
+//   //   data: {
+//   //     profile: {
+//   //       key: 'key',
+//   //       authorEmail: 'authorEmail',
+//   //       authorName: 'authorName',
+//   //     },
+//   //     hooks: {
+//   //       rootInstancePost: "() => {return 'rootInstancePost'}",
+//   //       pageInstancePost: () => {return 'pageInstancePost'},
+//   //       componentInstancePost: () => {return 'componentInstancePost'},
+//   //     },
+//   //     fnx: () => {
+//   //       clog('fnx')
+//   //     }
+//   //   }
+//   // })
 
-  // res.end({   // x
-  //       profile: {
-  //         key: 'key',
-  //         authorEmail: 'authorEmail',
-  //         authorName: 'authorName',
-  //       },
-  //       hooks: {
-  //         rootInstancePost: "() => {return 'rootInstancePost'}",
-  //         pageInstancePost: () => {return 'pageInstancePost'},
-  //         componentInstancePost: () => {return 'componentInstancePost'},
-  //       },
-  //       fnx: () => {
-  //         clog('fnx')
-  //       }})
-
-
-  res.status(200).json({
-    code: 0,
-    message: 'ok',
-    data: {
-      profile: {
-        key: 'key',
-        authorEmail: 'authorEmail',
-        authorName: 'authorName',
-      },
-      hooks: {
-        rootInstancePost: "() => {return 'rootInstancePost'}",
-        pageInstancePost: "() => {return 'pageInstancePost'}",
-        componentInstancePost: "() => {return 'componentInstancePost'}",
-      },
-      fnx: "() => {clog('fnx')}"
-    }
-})
-
-  // res.write('str')   // ok
-
-  // res.write({k: 's'})   // x
-  // res.end()
-
-  // res.send('sss') // ok
-  // res.send([1, 2]) // ok
-  // res.send({k: 2,b: true, y: () => {}}) // x
+//   // res.end({   // x
+//   //       profile: {
+//   //         key: 'key',
+//   //         authorEmail: 'authorEmail',
+//   //         authorName: 'authorName',
+//   //       },
+//   //       hooks: {
+//   //         rootInstancePost: "() => {return 'rootInstancePost'}",
+//   //         pageInstancePost: () => {return 'pageInstancePost'},
+//   //         componentInstancePost: () => {return 'componentInstancePost'},
+//   //       },
+//   //       fnx: () => {
+//   //         clog('fnx')
+//   //       }})
 
 
-})
-.post(cors.corsWithOptions, (req, res) => {
-  res.status(200).json({
-    code: 0,
-    message: "ok",
-    data: {},
-  })
-})
-.put(cors.corsWithOptions, (req, res) => {
-  res.send('put')
-})
-.delete(cors.corsWithOptions, (req, res) => {
-  res.send('delete')
-})
+//   res.status(200).json({
+//     code: 0,
+//     message: 'ok',
+//     data: {
+//       profile: {
+//         key: 'key',
+//         authorEmail: 'authorEmail',
+//         authorName: 'authorName',
+//       },
+//       hooks: {
+//         rootInstancePost: "() => {return 'rootInstancePost'}",
+//         pageInstancePost: "() => {return 'pageInstancePost'}",
+//         componentInstancePost: "() => {return 'componentInstancePost'}",
+//       },
+//       fnx: "() => {clog('fnx')}"
+//     }
+// })
+
+//   // res.write('str')   // ok
+
+//   // res.write({k: 's'})   // x
+//   // res.end()
+
+//   // res.send('sss') // ok
+//   // res.send([1, 2]) // ok
+//   // res.send({k: 2,b: true, y: () => {}}) // x
+
+
+// })
+// .post(cors.corsWithOptions, (req, res) => {
+//   res.status(200).json({
+//     code: 0,
+//     message: "ok",
+//     data: {},
+//   })
+// })
+// .put(cors.corsWithOptions, (req, res) => {
+//   res.send('put')
+// })
+// .delete(cors.corsWithOptions, (req, res) => {
+//   res.send('delete')
+// })
 
 
 module.exports = router;
