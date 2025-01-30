43829f01174829853d1c0847521b6c5785a933db
feigebaobei <18515195415@163.com>
2025-01-30 21:32:56 +0800

    feat(constructor): grid可以支持item的样式

--- packages/constructor/src/app/components/grid/grid.component.ts
+++ packages/constructor/src/app/components/grid/grid.component.ts
@@ -30,7 +30,10 @@ interface GridData {
 export class GridComponent {
   @Input() data!: GridData
   curPage: Page
-  compArr: (Comp | undefined)[] // 使用它
+  compArr: {
+    comp: Comp | undefined
+    styleObj: O
+  }[]
   show: B
   constructor(
     private pageService: PageService,
@@ -44,11 +47,25 @@ export class GridComponent {
     shareEvent.on(creatEventName('Grid', this.data.ulid, 'items', 'add'), (obj) => {
       let key = this.createSlotsKey(this.data.items.length - 1)
       this.data.slots[key] = ''
-      this.compArr.push(undefined)
+      this.compArr.push({
+        comp: undefined,
+        styleObj: {
+          'grid-column-start': this.data.items[this.data.items.length - 1]['gridColumnStart'],
+          'grid-row-start': this.data.items[this.data.items.length - 1]['gridRowStart'],
+          'grid-row-end': this.data.items[this.data.items.length - 1]['gridRowEnd'],
+          'grid-area': this.data.items[this.data.items.length - 1]['gridArea'],
+          'justify-self': this.data.items[this.data.items.length - 1]['justifySelf'],
+          'align-self': this.data.items[this.data.items.length - 1]['alignSelf'],
+        }
+      })
       this.componentService.reqUpdateComponent('slots', key, '', this.data.ulid)
     })
     shareEvent.on(creatEventName('Grid', this.data.ulid, 'items', 'remove'), ({item, index}) => {
-      this.compArr[index] = undefined
+      // this.compArr[index] = undefined
+      this.compArr[index] = {
+        comp: undefined,
+        styleObj: {}
+      }
       this.componentService.removeSlots(`${index}_`)
       this.componentService.reqRemoveSlots(`${index}_`)
     })
@@ -72,7 +89,16 @@ export class GridComponent {
         { area: 'slots', slotKey }, 
         {x: 0, y: 0, w: compGridLayout.w, h: compGridLayout.h, noResize: compGridLayout.noResize},
       )
-      this.compArr[index] = comp
+      this.compArr[index] = {comp,
+        styleObj: {
+          'grid-column-start': this.data.items[index]['gridColumnStart'],
+          'grid-row-start': this.data.items[index]['gridRowStart'],
+          'grid-row-end': this.data.items[index]['gridRowEnd'],
+          'grid-area': this.data.items[index]['gridArea'],
+          'justify-self': this.data.items[index]['justifySelf'],
+          'align-self': this.data.items[index]['alignSelf'],
+        }
+      }
       this.data.slots[slotKey] = comp.ulid
       this.componentService.mountComponent(this.curPage.ulid, comp)
       this.componentService.reqCreateComponent(comp)
@@ -83,8 +109,8 @@ export class GridComponent {
   }
   deleteCompH(ulid: ULID, index: N) {
     this.compArr = this.compArr.map(item => {
-      if (item?.ulid === ulid) {
-        return undefined
+      if (item.comp?.ulid === ulid) {
+        return {comp: undefined, styleObj: {}}
       } else {
         return item
       }
@@ -98,7 +124,19 @@ export class GridComponent {
   ngOnInit() {
     let tree = this.componentService.getTree(this.curPage.ulid)
     this.data.items.forEach((item, index) => {
-      this.compArr.push(tree?.find(this.data.slots[this.createSlotsKey(index)])?.value)
+      let compT = tree?.find(this.data.slots[this.createSlotsKey(index)])?.value
+      if (compT) {
+        this.compArr.push({comp: compT, styleObj: {
+          'grid-column-start': item['gridColumnStart'],
+          'grid-row-start': item['gridRowStart'],
+          'grid-row-end': item['gridRowEnd'],
+          'grid-area': item['gridArea'],
+          'justify-self': item['justifySelf'],
+          'align-self': item['alignSelf'],
+        }})
+      } else {
+        this.compArr.push({comp: undefined, styleObj: {}})
+      }
     })
     this.listen()
     asyncFn(() => {
