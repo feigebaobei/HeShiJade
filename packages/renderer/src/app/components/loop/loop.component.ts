46f483dae9da6c9c15d24c3b9d5723153a53843e
feigebaobei <18515195415@163.com>
2025-03-08 16:09:11 +0800

    feat(renderer): 渲染出视图

--- packages/renderer/src/app/components/loop/loop.component.ts
+++ packages/renderer/src/app/components/loop/loop.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, computed, Input } from '@angular/core';
+import { asyncFn, createChildKey } from 'src/helper/index'
+import { pool } from 'src/helper/pool';
+// type
+import type { Component as Comp, componentInstanceData } from 'src/types/component'
+import type { ULID } from 'src/types';
+import type { B, S, N, A, O } from 'src/types/base';
+import { ComponentService } from 'src/app/service/component.service';
+
+let clog = console.log
 
 @Component({
   selector: 'app-loop',
@@ -8,5 +17,98 @@ import { Component } from '@angular/core';
   styleUrl: './loop.component.sass'
 })
 export class LoopComponent {
+  @Input() data!: componentInstanceData
+  compArr: componentInstanceData[]
+  loopValue: componentInstanceData['props'][]
+  childComp: Comp | undefined
+  constructor(private componentService: ComponentService) {
+    // for dev
+    this.loopValue = [
+      {
+        error: false,
+        placeholder: '请输入',
+        showGlowStyle: true,
+        size: '',
+        styleType: 'default',
+        value: '',
+      },
+      {
+        error: false,
+        placeholder: '请输入',
+        showGlowStyle: true,
+        size: '',
+        styleType: 'default',
+        value: '',
+      },
+      {
+        error: false,
+        placeholder: '请输入',
+        showGlowStyle: true,
+        size: '',
+        styleType: 'default',
+        value: '',
+      },
+    ];
+    this.compArr = []
+    this.childComp = undefined
+  }
+  setProps(o: O) {
+    Object.entries(o).forEach(([k, v]) => {
+      this.data.props[k] = v
+    })
+  }
+  setLoopValue(a: componentInstanceData['props'][]) {
+    this.loopValue = a
+  }
+  setLoopValueByIndex(v: componentInstanceData['props'], index: N) {
+    this.loopValue[index] = v
+  }
+  objArr = computed(() => {
+    if (this.childComp) {
+      return this.loopValue.map(item => {
+        return {
+          ...this.childComp,
+          props: item,
+        } as Comp
+      })
+    } else {
+      return []
+    }
+  })
+  ngOnChanges() {
+    pool.trigger(this.data.ulid, 'postComponentNgOnChanges', undefined, this)
+  }
+  ngOnInit() {
+    pool.register(this.data.ulid, this, this.data.behavior)
+    pool.trigger(this.data.ulid, 'postComponentNgOnInit', undefined, this)
+    // asyncFn(() => {}).then(() => {})
+    let tree = this.componentService.getTreeByKey()
+    let ulid = this.data.slots['body']
+    if (ulid) {
+      this.childComp = tree?.find(ulid)?.value
+      clog('childComp', this.childComp)
+      // if (compT) {
+
+      //   // this.loopValue = {
+      //   //   ...compT,
+      //   // }
+      //   // this.loopValue.forEach(item => {
+      //   //   this.compArr.push
+      //   // })
+      // }
+    }
+    clog('objArr', this.objArr())
+  }
+  ngDoCheck() {
+    pool.trigger(this.data.ulid, 'postComponentNgDoCheck', undefined, this)
+  }
+  ngAfterViewInit() {
+    pool.trigger(this.data.ulid, 'postComponentNgAfterViewInit', undefined, this)
+    pool.resolveComponentRender(this.data.pageUlid, this.data.ulid)
+  }
+  ngOnDestroy() {
+    pool.trigger(this.data.ulid, 'postComponentNgOnDestroy', undefined, this)
+    pool.unRegister(this.data.ulid)
+  }
 
 }
